bb34ae4d45e730132a147ab6d13443921f93b87c
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-12-17 01:43:28 +0100

    created basic button

---
+++ src/components/Keycap/Keycap.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Top, Wrapper } from "./styled";
+
+export default function Keycap() {
+ return (
+  <Wrapper>
+   <Top><span>K</span></Top>
+  </Wrapper>
+ );
+}
