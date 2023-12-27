3f53c5e01f414abedbd4e8788bdf5384c17b6feb
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-12-27 17:49:40 +0100

    removed extend of shadows

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,6 @@ export default {
   ],
   theme: {
     extend: {
-      boxShadow: {
-        'keycap': 'inset 0 10px 10px 5px #ffffff60, inset 0 0 10px 0.5vw #000000aa, 0 5px 10px 0px black',
-        'keycapActive': 'inset 0 10px 10px 5px #ffffff50, inset 0 0 10px 0.5vw #3e3e3e, 0 2px 10px 0px black',
-        'keycapTop': 'inset 0 -2px 2px 2px #ffffff20, inset 0 2px 5px 2px #00000030, inset 0 0 10px 5px #ffffff15',
-        'light': '',
-      },
     },
   },
   plugins: [],
